42fd983cbd285e3746a86c90b5af3b2e8a886b6d
Vincent van Proosdij <mini32@gmail.com>
2020-04-14 17:19:50 +0200

    cc

--- src/Stateless.Web/IStatemachineDefinition.cs
+++ src/Stateless.Web/IStatemachineDefinition.cs
@@ -1,10 +1,14 @@
 ﻿namespace Stateless.Web
 {
+    using System;
+
     public interface IStatemachineDefinition
     {
         string Name { get; }
 
         string InitialState { get; }
+
+        TimeSpan? Ttl { get; }
 
         StateMachine CreateInstance(StateMachineContext instance, ITransitionDispatcher dispatcher);
     }
