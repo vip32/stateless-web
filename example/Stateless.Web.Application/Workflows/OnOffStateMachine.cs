8c076414a971fd510f1ae23d9effd0713b1c614f
Vincent van Proosdij <mini32@gmail.com>
2020-04-14 13:03:22 +0200

    move

--- example/Stateless.Web.Application/Workflows/OnOffStateMachine.cs
+++ example/Stateless.Web.Application/Workflows/OnOffStateMachine.cs
@@ -1,5 +1,8 @@
 ﻿namespace Stateless.Web
 {
+    using System.Threading.Tasks;
+    using Microsoft.Extensions.Logging;
+
     public static class OnOffStateMachine
     {
         public const string Name = "onoff";
@@ -16,6 +19,38 @@
             public const string Switch = "switch";
             public const string Break = "break";
             public const string Repair = "repair";
+        }
+
+        public class BrokenStateTransitionHandler : IStateTransitionHandler
+        {
+            private readonly ILogger<EchoStateTransitionHandler> logger;
+
+            public BrokenStateTransitionHandler(ILogger<EchoStateTransitionHandler> logger)
+            {
+                this.logger = logger;
+            }
+
+            public bool CanHandle(StateMachine stateMachine)
+            {
+                return stateMachine.Context.State.SafeEquals(States.Broken);
+            }
+
+            public Task OnEntryAsync(StateMachine stateMachine)
+            {
+                this.logger?.LogInformation($"{stateMachine.Context.Trigger} > PLEASE CALL A HANDYMAN");
+
+                return Task.CompletedTask;
+            }
+
+            public Task OnExitAsync(StateMachine stateMachine)
+            {
+                if (stateMachine.Context.Trigger.SafeEquals(Triggers.Repair))
+                {
+                    this.logger?.LogInformation($"{stateMachine.Context.Trigger} > PRAISE TO THE HANDYMAN");
+                }
+
+                return Task.CompletedTask;
+            }
         }
     }
 }
